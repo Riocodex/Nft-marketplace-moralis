b6538aeb75d3270a8dccbab480abf4c00d6f8f93
Riocodex <onwukachibike@gmail.com>
2022-11-04 17:22:55 +0100

    updated

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,16 @@
- 
+ import { ConnectButton } from "web3uikit";
+ import Link from "next/link"
+
+
+const Header = () => {
+  return (
+        <nav>
+            <Link href="/">
+                <a> NFT Marketplace</a>
+            </Link>
+            <ConnectButton/>
+        </nav>
+  )
+}
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
 
 function MyApp({ Component, pageProps }) {
   return (
+    //initalizeOnMoun = false cuz we dont want servers
     <MoralisProvider initializeOnMount={false}>
+        <Header/>
         <Component {...pageProps} />
     </MoralisProvider>
     
