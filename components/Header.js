9f017410d4f066882e7e10c98a252f8f58dcade3
Riocodex <onwukachibike@gmail.com>
2022-11-04 17:59:07 +0100

    added styling

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,18 @@
 
 const Header = () => {
   return (
-        <nav>
-            <Link href="/">
-                <a> NFT Marketplace</a>
-            </Link>
-            <ConnectButton/>
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+           <div className="flex flex-row items-center">
+           <Link href="/">
+                    <p className="mr-4 p-6">Home</p>
+                </Link>
+                <Link href="/sell-nft">
+                   <p className="mr-4 p-6">Sell NFT</p>
+                </Link>
+            <ConnectButton moralisAuth={false}/>
+            {/* i put moralis auth off above to ensure we do not connect with moralis database */}
+           </div>
         </nav>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-         <Head>
-        <title>NFT Marketplace</title>
-        <meta name="description" content="NFT Marketplace" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+         
 
      Hi!
     </div>
